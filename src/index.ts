b921a212e618459e290d4c84a92ad8228b96519e
Jiří Vrba <jiri@vrba.dev>
2022-11-13 23:20:33 +0100

    Implement a method for creating random bigints

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,26 @@
+import crypto from "node:crypto";
+
 // Find s and d such that n − 1 = 2^s * d by factorizing out powers of 2
 const findSD = (n: bigint): { s: bigint, d: bigint } => {
     let s = 0n;
     let d = (n - 1n);
 
-    while (d % 2n === 0n) {
+    // While the d is even
+    while ((d & 1n) === 0n) {
         s++;
-        d /= 2n; // TODO: Can this be speeded by using bit shifting hacks?
+        d >>= 1n; // Faster than division by 2
     }
 
     return { s, d }
+}
+
+// Return a random bigint within the specified lower and upper bounds
+const random = (low: bigint, high: bigint): bigint => {
+    const range = high - low;
+    const length = range.toString(2).length; // Bigint doesn't support log2
+    const bytes = crypto.getRandomValues(new Uint8Array(length));
+
+    return low + bytes.reduce((acc, byte) => ((acc + BigInt(byte & 1)) << 1n) , BigInt(0));
 }
 
 /**
@@ -32,7 +44,6 @@ export const test = (n: bigint, k: number): boolean => {
 
     // let s > 0 and d odd > 0 such that n − 1 = 2^s * d 
     const { s, d } = findSD(n);
-
 
     return false;
 };
